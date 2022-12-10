01b9fbba411abb21ebff333d575470b2be1485c9
TheUnfocused <mutnba@gmail.com>
2022-12-10 13:12:48 -0800

    Day 9 is solved

--- day9/day9.py
+++ day9/day9.py
@@ -1,43 +1,54 @@
-def moveKnot(knot, direction):
+def areNotTouching(head, tail):
+    return max(abs(head[0] - tail[0]), abs(head[1] - tail[1])) > 1
+
+def moveHead(knot, direction):
     match direction:
-            case 'R':
-                knot[0] = (knot[0] + 1) % 5
-            case 'L':
-                knot[0] = (knot[0] - 1) % 5
-            case 'U':
-                knot[1] = (knot[1] + 1) % 4
-            case 'D':
-                knot[1] = (knot[1] - 1) % 4 
-    return knot
+        case "R":
+            return (knot[0], knot[1] + 1)
+        case "L":
+            return (knot[0], knot[1] - 1)
+        case "U":
+            return (knot[0] + 1, knot[1])
+        case "D":
+            return (knot[0] - 1, knot[1])
+        case _:
+            return knot
+
+def moveTail(head, tail):
+    tail_first = tail[0]
+    if head[0] > tail[0]:
+        tail_first += 1
+    elif head[0] < tail[0]:
+        tail_first -= 1
+    
+    tail_second = tail[1]
+    if head[1] > tail[1]:
+        tail_second += 1
+    elif head[1] < tail[1]:
+        tail_second -= 1
+    
+    return (tail_first, tail_second)
+
+def simulateRope(rope, moves):
+    grid = set()
+
+    for move in moves:
+        direction, magnitude = move.split(" ")
+        for _ in range(int(magnitude)):
+            rope[0] = moveHead(rope[0], direction)
+            for i in range(1, len(rope)):
+                if areNotTouching(rope[i - 1], rope[i]):
+                    rope[i] = moveTail(rope[i - 1], rope[i])
+
+            grid.add((rope[-1][0], rope[-1][1]))
+    
+    return len(grid)
 
 with open("day9/input.txt", "r") as day9_input:
-    data = day9_input.readlines()
+    moves = day9_input.readlines()
 
-grid = [[False]*6 for i in range(5)]
-head = [0, 0]
-tail = [0, 0]
+rope_part1 = [(0, 0) for _ in range(2)]
+print("Part 1: " + str(simulateRope(rope_part1, moves)))
 
-for move in data:
-    direction = move[0]
-    magnitude = int(move[2])
-    while magnitude > 0:
-        areTheyDiagonal = abs(head[0] - tail[0]) >= 1 and abs(head[1] - tail[1]) >= 1
-        head = moveKnot(head, direction)
-        if abs(head[0] - tail[0]) > 1 or abs(head[1] - tail[1]) > 1:
-            if areTheyDiagonal:
-                if direction == 'R' or direction == 'L':
-                    tail = moveKnot(tail, 'U') if head[1] - tail[1] > 0 else moveKnot(tail, 'D')
-                else:
-                    tail = moveKnot(tail, 'R') if head[0] - tail[0] > 0 else moveKnot(tail, 'L')
-            tail = moveKnot(tail, direction)
-        
-        grid[tail[0]][tail[1]] = True
-        magnitude -= 1
-
-total_visited_part1 = 0
-for i in range(5):
-    for j in range(6):
-        if grid[i][j]:
-            total_visited_part1 += 1
-
-print("Part 1: " + str(total_visited_part1))
+rope_part2 = [(0, 0) for _ in range(10)]
+print("Part 2: " + str(simulateRope(rope_part2, moves)))
